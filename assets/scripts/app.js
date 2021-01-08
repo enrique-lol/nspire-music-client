5cf27c0fd2892937a3b4abaefc93122ad29e0f9a
enrique-lol <enrique.memije.pro@gmail.com>
2021-01-07 22:55:36 -0600

    Front-end Testing Add code that tests the use of buttons to hide and show content Add sign-up and sign-in forms Add event listeners for form-submitals

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,22 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const events = require('./events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
+  // Below test of site navigation
+  $('.page').hide()
+  $('.p1').show()
+  $('#next-from-p1').on('click', events.nextFromPageOne)
+  $('#previous-from-p2').on('click', events.previousFromPageTwo)
+  $('#next-from-p2').on('click', events.nextFromPageTwo)
+  $('#previous-from-p3').on('click', events.previousFromPageThree)
+  $('#next-from-p3').on('click', events.nextFromPageThree)
+  $('#previous-from-p4').on('click', events.previousFromPageFour)
+  // below: testing the login navigation to next page
+  $('#sign-in-form').on('submit', events.nextFromPageOne)
   // your JS code goes here
 })
